e99042261990593197c1f178ac46c5782a2c2730
Thiago Delgado Pinto <thiago_dp@yahoo.com.br>
2026-07-15 01:07:37 -0300

    Improve example 01

--- examples/01-hello-world/index.php
+++ examples/01-hello-world/index.php
@@ -1,10 +1,23 @@
 <?php
 // require_once 'vendor/autoload.php';
-require_once '../../src/router.php';
+require_once __DIR__ . '/../../src/RouteToRegexResult.php';
+require_once __DIR__ . '/../../src/Router.php';
 
 $app = new \phputil\router\Router();
+
+$app->setDebugMode( true );
+
+$app->use( function( $req, $res ) {
+    $res->header( 'Foo', 'bar' );
+} );
+
 $app->get('/', function( $req, $res ) {
     $res->send( 'Hello, World!' );
 } );
+
+$app->get('/error', function( $req, $res ) {
+    throw new Exception( 'Ouch' );
+} );
+
 $app->listen();
 ?>
